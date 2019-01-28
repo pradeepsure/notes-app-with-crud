61a187ac5b9a478ffc05d0fa2e115ad7e713fbce
VenkataNagaSaiPradeepKumar.Sure <venkatanagasaipradeepkumar.sure@cognizant.com>
2019-01-28 12:32:30 +0000

    some changes related to ui

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -8,18 +8,12 @@ import takeANote from '../images/takenote.jpg';
 function WelcomePage(props) {
     return (
         <Fragment>
-            <MDBContainer>
-                <MDBCarousel activeItem={1}  showControls={false} showIndicators={false} className="z-depth-1">
-                    <MDBCarouselInner>
-                        <MDBCarouselItem itemId="1">
-                            <MDBView>
-                                <img className="d-block w-100" src={takeANote} alt="First slide" />                                
+            
+                            <MDBView  >
+                                <img className="d-block w-50" src={takeANote} alt="First slide" />                                
                                 <MDBMask overlay="black-light" />
                             </MDBView>
-                        </MDBCarouselItem>                     
-                    </MDBCarouselInner>
-                </MDBCarousel>
-            </MDBContainer>
+                        
         </Fragment>
     );
 
